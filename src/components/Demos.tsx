5b149deb3dcfbe2138b4127a8f49f673410474a4
Nikhil Massand <nikhil.massand@gmail.com>
2021-06-01 13:01:25 -0400

    added more components and continued styling

---
+++ src/components/Demos.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Demos() {
+  return(
+    <div id="demos" className="demosContainer">
+      <div className="producer">
+        {/* Put the gif here */}
+        <button>
+          <a href="">Install Producer Demo</a>
+        </button>
+      </div>
+
+      <div className="consumer">
+        {/* Put the gif here */}
+        <button>
+          <a href="">Install Consumer Demo</a>
+        </button>
+      </div>
+
+    </div>
+  )
+}
+
+export default Demos;
--- src/components/TitleCard.tsx
+++ src/components/TitleCard.tsx
@@ -5,7 +5,7 @@ function TitleCard() {
     <div className="titleCard">
       {/* <img src="assets/logo-vert.png" width="600" alt="" /> */}
       <img id="logo" src="assets/logo.svg" height="200" width="600" alt="" />
-      <h3>An npm package for JavaScript developers that facilitates connections between React applications and Kafka cluster through the use of WebSockets
+      <h3 id="titleDescript">An npm package for JavaScript developers that facilitates event-driven data streaming between React applications and Kafka clusters through the use of WebSockets
       </h3>
       <a href="https://github.com/oslabs-beta/AtomicKafka">
         <button id="install-btn">
