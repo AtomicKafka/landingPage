6ab3bcffa3fdfaaa9bc27d14a94474f0f486e090
Vicki Yang <vickiwyang@gmail.com>
2021-06-01 20:55:22 -0500

    fixed webpack production issues

--- server.js
+++ server.js
@@ -23,6 +23,16 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname + '/index.html'))
 })
 
+if (process.env.NODE_ENV === 'production') {
+  // statically serve everything in the build folder on the route '/build'
+  app.use('/build', express.static(path.join(__dirname, './build')));
+  // serve index.html on the route '/'
+  app.get('/', (req, res) => {
+    return res.status(200).sendFile(path.join(__dirname, './index.html'));
+  });
+}
+
+
 //Global 404 handler
 app.use('*', (req, res) => {
   return res.status(404).send('********** GLOBAL BAD REQUEST / 404 ERROR **********');
