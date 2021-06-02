ebb09116badde455b12f6227ed50e757a93d16ba
Vicki Yang <vickiwyang@gmail.com>
2021-06-02 15:40:09 -0500

    changed TitleCard description

--- src/components/TitleCard.tsx
+++ src/components/TitleCard.tsx
@@ -5,8 +5,7 @@ function TitleCard() {
     <div className="titleCard">
       {/* <img src="assets/logo-vert.png" width="600" alt="" /> */}
       <img id="logo" src="assets/logo.svg" height="200" width="600" alt="" />
-      <h3 id="titleDescript">An npm package for JavaScript developers that facilitates event-driven data streaming between React applications and Kafka clusters through the use of WebSockets
-      </h3>
+      <h3 id="titleDescript">A lightweight npm package developed to simplify the process of establishing bi-directional, real-time data streaming with Apache Kafka in your web app</h3>
       <a href="https://github.com/oslabs-beta/AtomicKafka">
         <button id="install-btn">
         Install
