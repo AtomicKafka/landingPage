a69df33e811a3071ff78337231990b906461b62a
Nikhil Massand <nikhil.massand@gmail.com>
2021-06-02 12:28:59 -0400

    additional svg styling

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -8,17 +8,26 @@ function Features() {
         <h3 className="featureHeading">Server-Side Library</h3>
         <div className="explainPlusBlock">
           <div className="codeExplain">
-            <p>Simply import Atomic Kafka server side library and initialize an instance using the port 
-            connected with the application's server. Ensure that your .env file is set up with either your Confluent 
-            credentials or docker image. See an example of what that should look like HERE.</p>
+            <p>Ensure that your .env file is set up with either your Confluent 
+              credentials or docker image. See an example of what that should look like in our readme 
+              <a href="https://github.com/oslabs-beta/AtomicKafka" className="linkLib">here</a>. 
+              Simply import Atomic Kafka server side library and initialize an instance using the port 
+              connected with the application's server. 
+            </p>
           </div>
           <div className="codeBlockContainer">
-            <img className="codeBlock" src="../../assets/serverside-1.svg" />
+            {/* <svg  width="100%"> */}
+            {/* <svg viewBox="0 0 200 auto"> */}
+              <img className="codeBlock" src="../../assets/serverside-1.svg" />
+            {/* </svg> */}
           </div>
         </div>
         <div className="explainPlusBlock">
           <div className="codeExplain">
-            <p>Blah blah blah do something here, download our code, star our package, hire us for jobs. Blah blah blah do something here, download our code, star our package, hire us for jobs. Blah blah blah do something here, download our code, star our package, hire us for jobs.</p>
+            <p>Create consumers using the newConsumer function and pass in the group id that the consumer should be listenening on.
+              By invoking socketConsume with the group id, event, and topic the socket instance will emit the consumed messages for the consumer
+              with the specified group id. The app can now listen for those emitted events on the front end.
+            </p>
           </div>
           <div className="codeBlockContainer">
             <img className="codeBlock" src="../../assets/serverside-2.svg" />
@@ -26,7 +35,10 @@ function Features() {
         </div>
         <div className="explainPlusBlock">
           <div className="codeExplain">
-          <p>Blah blah blah do something here, download our code, star our package, hire us for jobs. Blah blah blah do something here, download our code, star our package, hire us for jobs. Blah blah blah do something here, download our code, star our package, hire us for jobs.</p>
+          <p>Create consumers using the newProducer function and pass in the topic that the producer will send messages to.
+              By invoking globalProduce with a topic the socket instance will listen for messages that are emitted from the frontend.
+              The function fileProduce, seen in the producer demo app, allows for a controlled send of a message from the server side based on the user's payload input.
+          </p>
           </div>
           <div className="codeBlockContainer">
             <img className="codeBlock" src="../../assets/serverside-3.svg" />
@@ -37,7 +49,9 @@ function Features() {
         <h3 className="featureHeading">Client-Side Library</h3>
         <div className="explainPlusBlock">
           <div className="codeExplain">
-          <p>Blah blah blah do something here, download our code, star our package, hire us for jobs. Blah blah blah do something here, download our code, star our package, hire us for jobs. Blah blah blah do something here, download our code, star our package, hire us for jobs.</p>
+          <p>
+            Import in the client library like so if using Typescript otherwise a normal import suffices if using Javascript (ES6+).
+          </p>
           </div>
           <div className="codeBlockContainer">
             <img className="codeBlock" src="../../assets/clientside-1.svg" />
@@ -45,7 +59,12 @@ function Features() {
         </div>
         <div className="explainPlusBlock">
           <div className="codeExplain">
-            <p>Blah blah blah do something here, download our code, star our package, hire us for jobs. Blah blah blah do something here, download our code, star our package, hire us for jobs. Blah blah blah do something here, download our code, star our package, hire us for jobs.</p>
+            <p>
+              Instantiate the client class within a consumer component passing in the uri that the socket should listen on. By defining
+              a callback that takes a payload and changes state however necessary within the function body. Finally, by invoking useInterval and 
+              consumer, with the socket event and callback as parameters to consumer the app will now be listening to incoming messages that are emitted
+              from the server side.
+            </p>
           </div>
           <div className="codeBlockContainer">
             <img className="codeBlock" src="../../assets/clientside-2.svg" />
@@ -53,7 +72,11 @@ function Features() {
         </div>
         <div className="explainPlusBlock">
           <div className="codeExplain">
-          <p>Blah blah blah do something here, download our code, star our package, hire us for jobs. Blah blah blah do something here, download our code, star our package, hire us for jobs. Blah blah blah do something here, download our code, star our package, hire us for jobs.</p>
+          <p>
+            When creating a producer component instantiate the client class with the appropriate uri that the socket will emit to.
+            Define a payload for the message that will be sent to the cluster and pass that into the client producer function along 
+            with the socket event that the payload will be sent to.
+          </p>
           </div>
           <div className="codeBlockContainer">
             <img className="codeBlock" src="../../assets/clientside-3.svg" />
