3085e3d12601ccc38ef02b626074d0b76a18a4c9
Vicki Yang <vickiwyang@gmail.com>
2021-06-03 15:24:31 -0500

    new logo

--- src/components/TitleCard.tsx
+++ src/components/TitleCard.tsx
@@ -4,7 +4,7 @@ function TitleCard() {
   return(
     <div className="titleCard">
       {/* <img src="assets/logo-vert.png" width="600" alt="" /> */}
-      <img id="logo" src="assets/logo.svg" height="200" width="600" alt="" />
+      <img id="logo" src="assets/newlogo.svg" height="200" width="600" alt="" />
       <h3 id="titleDescript">A lightweight npm package developed to simplify the process of establishing bi-directional, real-time data streaming with Apache Kafka in your web app</h3>
       <a href="https://github.com/oslabs-beta/AtomicKafka" target="_blank">
         <button id="install-btn">
